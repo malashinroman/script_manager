ee51a94b9a8ef363bcf3a239eb9230e5bd4508c5
Roman Malashin <malashinroman@mail.ru>
2022-06-28 12:02:03 +0300

    add dict log option

--- func/wandb_logger.py
+++ func/wandb_logger.py
@@ -1,18 +1,17 @@
 # empty = None
+import os.path as osp
+from copy import deepcopy
+
 import wandb
+from local_config import WANDB_LOGIN
 
 # from torch.utils.tensorboard import SummaryWriter
 from tensorboardX.writer import SummaryWriter
-
-import os.path as osp
-
-from local_config import WANDB_LOGIN
 
 __WANDB_LOG__ = None
 
 
 class WandbLogger(object):
-
     def __init__(self, args, wandb_entity):
         super().__init__()
         self._args = args
@@ -30,8 +29,10 @@ class WandbLogger(object):
 
         if args.wandb_project_name is not None:
             self.use_wandb = True
-            wandb.init(project=args.wandb_project_name, entity=wandb_entity, config=args)
-            wandb.run.name = args.tag + '_' + wandb.run.name
+            wandb.init(
+                project=args.wandb_project_name, entity=wandb_entity, config=args
+            )
+            wandb.run.name = args.tag + "_" + wandb.run.name
         else:
             self.use_wandb = False
 
@@ -48,19 +49,35 @@ def update_args_from_wandb(args):
         return args
 
 
-def write_wandb_scalar(tag, scalar_value, global_step=None):
+def write_wandb_scalar(tag, scalar_value=None, global_step=None):
     global __WANDB_LOG__
     logged = 0
-    if __WANDB_LOG__.use_tensorboard:
-        __WANDB_LOG__._writer.add_scalar(tag, scalar_value, global_step)
-        logged = 1
+    if __WANDB_LOG__ is not None:
+        if __WANDB_LOG__.use_tensorboard:
+            if type(tag) is dict:
+                log_dict = deepcopy(tag)
+                tag = list(log_dict.keys())[0]
+                scalar_value = log_dict[tag]
+                if "global_step" in log_dict:
+                    global_step = log_dict["global_step"]
+            __WANDB_LOG__._writer.add_scalar(tag, scalar_value, global_step)
+            logged = 1
 
-    if __WANDB_LOG__.use_wandb:
-        wandb.log({tag: scalar_value, 'global_step': global_step})
-        logged = 1
+        if __WANDB_LOG__.use_wandb:
+            if type(tag) is dict:
+                log_dict = deepcopy(tag)
+
+                log_dict["global_step"] = global_step
+                wandb.log(log_dict)
+            else:
+                wandb.log({tag: scalar_value, "global_step": global_step})
+            logged = 1
 
     if not logged:
-        print("WARNING: write_wandb_scalar has no effect, because logger is not initialized")
+        print(
+            "WARNING: write_wandb_scalar has no effect, because logger is not initialized"
+        )
+
 
 # def write_wandb_video(tag, frames, global_step=None):
 #     global __WANDB_LOG__
@@ -75,6 +92,7 @@ def write_wandb_scalar(tag, scalar_value, global_step=None):
 #
 #     if not logged:
 #         print("WARNING: write_wandb_scalar has no effect, because logger is not initialized")
+
 
 def prepare_wandb(args):
     init_wandb_logger(args, WANDB_LOGIN)
