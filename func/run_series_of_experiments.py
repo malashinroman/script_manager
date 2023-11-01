17d4244013dbebad75eb8160425ea096f42151c7
Roman Malashin <Roman Malashin>
2023-11-01 15:18:13 +0300

    cuda async load balance support

--- func/run_series_of_experiments.py
+++ func/run_series_of_experiments.py
@@ -28,26 +28,24 @@ def get_command_params_environs(cmd: str):
     return command_params, my_env
 
 
-def run_async(run_list=[], parallel_num=2, cwd="."):
-    """
-    Run a list of commands in parallel.
-    param run_list     : list of commands to be run
-    param parallel_num : number of commands to be run in parallel
-    param cwd          : current working directory
-    return             :
-    """
-    if parallel_num == 1:
-        for cmd in run_list:
-            print(f"cmd:{cmd}")
+def get_attach_gpu_dict(run_list=[]):
+    # convert run_list to dict with CUDA_VISIBLE_DEVICES inex as a key
+    run_dict = {}
+    unattached_list = []
+    for cmd in run_list:
+        _, my_env = get_command_params_environs(cmd)
+        if "CUDA_VISIBLE_DEVICES" in my_env:
+            gpu = int(my_env["CUDA_VISIBLE_DEVICES"])
+            if gpu not in run_dict:
+                run_dict[gpu] = []
+            run_dict[gpu].append(cmd)
+        else:
+            unattached_list.append(cmd)
+    return run_dict, unattached_list
 
-            """launch the process"""
-            command_params, my_env = get_command_params_environs(cmd)
-            subprocess.call(command_params, cwd=cwd, env=my_env)
-            # subprocess.Popen(cmd.split(" "),shell=True)
-        return
 
+def run_simple_parallel(run_list, parallel_num=2, cwd="."):
     progresses = []
-    assert len(run_list) >= parallel_num, "run_list should be longer than parallel_num"
     for cmd in run_list[:parallel_num]:
         command_params, my_env = get_command_params_environs(cmd)
         progresses.append(subprocess.Popen(command_params, cwd=cwd, env=my_env))
@@ -73,3 +71,79 @@ def run_async(run_list=[], parallel_num=2, cwd="."):
     for p in progresses:
         p.wait()
     print("Everything finished!")
+
+
+def run_process_per_gpu(gpu_cmd_dict, parallel_num=2, cwd="."):
+    gpu_num = len(gpu_cmd_dict)
+
+    progresses_dict = {g: [] for g in range(gpu_num)}
+    if isinstance(parallel_num, int):
+        proc_per_gpu = [parallel_num // gpu_num] * gpu_num
+
+    for i in range(gpu_num):
+        for j in range(proc_per_gpu[i]):
+            cmd = gpu_cmd_dict[i][j]
+            command_params, my_env = get_command_params_environs(cmd)
+            progresses_dict[i].append(
+                subprocess.Popen(command_params, cwd=cwd, env=my_env)
+            )
+            gpu_cmd_dict[i] = gpu_cmd_dict[i][1:]
+
+    for gpu_ind, gpu_progresses in progresses_dict.items():
+        for progress in gpu_progresses:
+            print(progress)
+
+    while sum([len(v) for _, v in gpu_cmd_dict.items()]) > 0:
+        for gpu_ind, gpu_progresses in progresses_dict.items():
+            for i in range(len(gpu_progresses)):
+                print(
+                    f"gpu {gpu_ind} process {i} running: {gpu_progresses[i].poll() is None}"
+                )
+                if not gpu_progresses[i].poll() is None:
+                    if len(gpu_cmd_dict[gpu_ind]) > 0:
+                        cmd = gpu_cmd_dict[gpu_ind][0]
+                        gpu_cmd_dict[gpu_ind] = gpu_cmd_dict[gpu_ind][1:]
+                        command_params, my_env = get_command_params_environs(cmd)
+                        gpu_progresses[i] = subprocess.Popen(
+                            command_params, cwd=cwd, env=my_env
+                        )
+                        print(
+                            f"gpu {gpu_ind} process {i} running: {gpu_progresses[i].poll() is None}"
+                        )
+        time.sleep(10)
+
+    print("All Threads are queued, let's see when they finish!")
+
+    for gpu_ind, gpu_progresses in progresses_dict.items():
+        for p in gpu_progresses:
+            p.wait()
+    print("Everything finished!")
+
+
+def run_async(run_list=[], parallel_num=2, cwd="."):
+    """
+    Run a list of commands in parallel.
+    param run_list     : list of commands to be run
+    param parallel_num : number of commands to be run in parallel
+    param cwd          : current working directory
+    return             :
+    """
+    # __import__('pudb').set_trace()
+    if parallel_num == 1:
+        for cmd in run_list:
+            print(f"cmd:{cmd}")
+
+            """launch the process"""
+            command_params, my_env = get_command_params_environs(cmd)
+            subprocess.call(command_params, cwd=cwd, env=my_env)
+            # subprocess.Popen(cmd.split(" "),shell=True)
+        return
+
+    assert len(run_list) >= parallel_num, "run_list should be longer than parallel_num"
+
+    gpu_attached, unattached_list = get_attach_gpu_dict(run_list)
+
+    if len(unattached_list) > 0:
+        run_simple_parallel(run_list, parallel_num, cwd)
+    else:
+        run_process_per_gpu(gpu_attached, parallel_num, cwd)
